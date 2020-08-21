18c76249b9166bbc602a7eb67667b6cc7f2d8c7b
Felipe P Covatti <felipepcovatti@gmail.com>
2020-08-21 18:45:21 -0300

    Implement products creation

--- src/modules/products/infra/typeorm/repositories/ProductsRepository.ts
+++ src/modules/products/infra/typeorm/repositories/ProductsRepository.ts
@@ -1,4 +1,4 @@
-import { getRepository, Repository, In } from 'typeorm';
+import { getRepository, Repository } from 'typeorm';
 
 import IProductsRepository from '@modules/products/repositories/IProductsRepository';
 import ICreateProductDTO from '@modules/products/dtos/ICreateProductDTO';
@@ -21,21 +21,48 @@ class ProductsRepository implements IProductsRepository {
     price,
     quantity,
   }: ICreateProductDTO): Promise<Product> {
-    // TODO
+    const product = this.ormRepository.create({ name, price, quantity });
+
+    await this.ormRepository.save(product);
+
+    return product;
   }
 
   public async findByName(name: string): Promise<Product | undefined> {
-    // TODO
+    const product = await this.ormRepository.findOne({ where: { name } });
+
+    return product;
   }
 
   public async findAllById(products: IFindProducts[]): Promise<Product[]> {
-    // TODO
+    const productsIds = products.map(product => product.id);
+    const foundProducts = await this.ormRepository.findByIds(productsIds);
+
+    return foundProducts;
   }
 
   public async updateQuantity(
     products: IUpdateProductsQuantityDTO[],
   ): Promise<Product[]> {
-    // TODO
+    const productsIds = products.map(({ id }) => id);
+
+    const foundProducts = await this.ormRepository.findByIds(productsIds);
+
+    const productsQuantityById = Object.assign(
+      {},
+      ...products.map(({ id, quantity }) => ({
+        [id]: quantity,
+      })),
+    );
+
+    const updatedProducts = foundProducts.map(foundProduct => ({
+      ...foundProduct,
+      quantity: productsQuantityById[foundProduct.id],
+    }));
+
+    await this.ormRepository.save(updatedProducts);
+
+    return updatedProducts;
   }
 }
 
